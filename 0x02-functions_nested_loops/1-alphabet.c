5291f46aa728208ad6ecfb9665c6fdbf5b759c6f
Adefash02 <adelekeabdulmalikaderemi@gmail.com>
2023-08-16 14:58:47 -0700

    ns

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- *print_alphabet - Make the alphabet
- *
- *Return: void
- */
+*print_alphabet - Make the alphabet
+*
+*Return: void
+*/
 void print_alphabet(void)
 {
 	char van;
-	for ( van = 'a'; van <= 'z'; van++)
-	_putchar(van);
+	for (van = 'a'; van <= 'z'; van++)
+		_putchar(van);
 	_putchar('\n');
 }
