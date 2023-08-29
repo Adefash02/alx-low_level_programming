86934340e532fa36b57c4f758dfbc762a0fc6ac5
Adefash02 <adelekeabdulmalikaderemi@gmail.com>
2023-08-29 06:53:05 -0700

    memset

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -10,23 +10,23 @@
  */
 void simple_print_buffer(char *buffer, unsigned int size)
 {
-	unsigned int i;
+        unsigned int i;
 
-	i = 0;
-	while (i < size)
-	{
-		if (i % 10)
-		{
-			printf(" ");
-                }
-		if (!(i % 10) && i)
+        i = 0;
+        while (i < size)
+        {
+                if (i % 10)
                 {
-			printf("\n");
+                        printf(" ");
                 }
-		printf("0x%02x", buffer[i]);
-		i++;
+                if (!(i % 10) && i)
+                {
+                        printf("\n");
+                }
+                printf("0x%02x", buffer[i]);
+                i++;
         }
-	printf("\n");
+        printf("\n");
 }
 
 /**
@@ -39,8 +39,9 @@ int main(void)
     char buffer[98] = {0x00};
 
     simple_print_buffer(buffer, 98);
-    *_memset(buffer, 0x01, 95);
+    _memset(buffer, 0x01, 95);
     printf("-------------------------------------------------\n");
-    simple_print_buffer(buffer, 98);
+    simple_print_buffer(buffer, 98);    
     return (0);
 }
+
