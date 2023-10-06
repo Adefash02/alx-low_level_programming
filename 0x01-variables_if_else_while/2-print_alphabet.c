575d9cc88d4b3aff6bd64b7823b50a7cc2dd92a4
Adefash02 <adelekeabdulmalikaderemi@gmail.com>
2023-10-06 16:17:27 -0700

    alphabelt

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - The task function
+* Return: 0 (success)
+*/
+
+int main(void)
+{
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+putchar(i);
+putchar('\n');
+
+return (0);
+}
