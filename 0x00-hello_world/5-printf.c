a0ba2b9b68aa48ad2a391e8c5c1d37f669c36cb2
Adefash02 <adelekeabdulmalikaderemi@gmail.com>
2023-10-06 01:51:45 -0700

    betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 /**
-*Main - Entry point
-*return: always 0 (SUCCESS)
+ *Main - this is printf function
+ *return: always 0 (SUCCESS)
 */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
