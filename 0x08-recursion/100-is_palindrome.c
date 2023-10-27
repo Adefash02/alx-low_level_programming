17a8279e5965588784f89bee485b9b6f1cb50912
Adefash02 <adelekeabdulmalikaderemi@gmail.com>
2023-10-26 19:19:35 -0700

    reursion update

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,7 +12,7 @@ int is_palindrome(char *s)
 {
 	if (*s == 0)
 		return (1);
-	return (check_pal(s, 0, _strlen_recursion(s)));
+	return (check_pal(s, 0, _strlen_recursion(s) - 1));
 }
 
 /**
@@ -24,6 +24,7 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 		return (0);
+	else
 	return (1 + _strlen_recursion(s + 1));
 }
 
@@ -36,9 +37,11 @@ int _strlen_recursion(char *s)
  */
 int check_pal(char *s, int i, int len)
 {
-	if (*(s + 1) != *(s + len - 1))
-		return (0);
-	if (i >= len)
-		return (1);
-	return (check_pal(s, i + 1, len - 1));
+	if (*(s + i) == *(s + len))
+	{
+		if (i == len || i == len + 1)
+			return (1);
+		return (0 + check_pal(s, i + 1, len - 1));
+	}
+	return (0);
 }
