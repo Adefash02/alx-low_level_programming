1eadfc8d587a46f5fe1ae7f544253f5d4b37f6d6
Adefash02 <adelekeabdulmalikaderemi@gmail.com>
2023-10-30 22:30:19 -0700

    nice

---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+malloc readme for 0x0A-argc_argv
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#define MAIN_H
+#include <stdio>
+
+int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int _atoi(char *s);
+int main(int argc, char *argv[]);
+int _putchar(char c);
+
+#endif
